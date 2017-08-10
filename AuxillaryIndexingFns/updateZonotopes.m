c6c83e6fbf142cbb1677f08d320a5ed258d4095c
bolyachevets <andrew.bolyachevets@utoronto.ca>
2017-08-10 13:56:18 -0700

    fixed backward reach set computation

--- AuxillaryIndexingFns/updateZonotopes.m
+++ AuxillaryIndexingFns/updateZonotopes.m
@@ -1,12 +1,15 @@
 % scaling and reach set updating for implementation with lower and upper
 % bound scalars on generators
 
-function [IC_z_generators_copy, alphas, scaledICs, R_zs, IC_z_copy, IC_c_copy] = ...
-    updateZonotopes(alpha_gx_low, alpha_gx_high, IC_z_generators_copy, IC_c_copy, alphas, dim, s, scaledICs, A_d, R_zs)
+function [IC_z_generators_copy, alphas, center_shift, scaledICs, R_zs, IC_z_copy, IC_c_copy] = ...
+    updateZonotopes(alpha_gx_low, alpha_gx_high, IC_z_generators_copy, IC_c_copy, alphas, center_shift, dim, s, scaledICs, A_d, R_zs)
          % add scalars from the current iteration to the vector of scalars
         for j=1:dim
             alphas(generatorIndex(dim, s)+j) = (alpha_gx_high(j) - alpha_gx_low(j))/2;
         end
+        
+        % update cumulative center shift
+        center_shift = center_shift + (A_d^(-1))^(s-1)*(IC_z_generators_copy*(alpha_gx_high + alpha_gx_low)/2);
         
         % update the center of the scaled initial set
         IC_c_copy = IC_c_copy + IC_z_generators_copy*(alpha_gx_high + alpha_gx_low)/2;
@@ -17,8 +20,7 @@ function [IC_z_generators_copy, alphas, scaledICs, R_zs, IC_z_copy, IC_c_copy] =
             IC_z_g = horzcat(IC_z_g, (alpha_gx_high(i) - alpha_gx_low(i))/2*IC_z_generators_copy(:,i));
         end
         IC_z_generators_copy = IC_z_g;
-
-
+        
         % construct scalings of initial sets
         IC_z_copy_mat = horzcat(IC_c_copy, IC_z_generators_copy);
         IC_z_copy = zonotope(IC_z_copy_mat);
